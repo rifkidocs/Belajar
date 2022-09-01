94ba07943f65e52acd4f54da4fbfdf602a0ec09c
rifkidocs <rifkiars@gmail.com>
2022-09-01 09:18:27 +0700

    konversi

--- Javascript/konversi.js
+++ Javascript/konversi.js
@@ -4,5 +4,9 @@ const value2 =  1;
 const total = value1 + value2;
 
 document.writeln(`<p>${total}</p>`);
+document.writeln(`<p>${isNaN(NaN)}</p>`);
+document.writeln(`<p>${isNaN("salah")}</p>`);
+document.writeln(`<p>${isNaN("9")}</p>`);
+document.writeln(`<p>${isNaN("9salah")}</p>`);
 
 //konversi
