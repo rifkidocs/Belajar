adc057350b07374d27641b9bec7ab8fd2832a300
rifkidocs <rifkiars@gmail.com>
2022-09-18 20:38:55 +0700

    input data

--- Visual Basic/Project 1/WindowsApp1/WindowsApp1/Form1.Designer.vb
+++ Visual Basic/Project 1/WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -25,6 +25,14 @@ Partial Class Form1
         Me.BtnStart = New System.Windows.Forms.Button()
         Me.Btn2 = New System.Windows.Forms.Button()
         Me.Btn3 = New System.Windows.Forms.Button()
+        Me.BtnInput = New System.Windows.Forms.Button()
+        Me.LabelFirstName = New System.Windows.Forms.Label()
+        Me.LabelLastName = New System.Windows.Forms.Label()
+        Me.LabelGender = New System.Windows.Forms.Label()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtGender = New System.Windows.Forms.TextBox()
+        Me.btnSubmit = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'BtnStart
@@ -32,7 +40,7 @@ Partial Class Form1
         Me.BtnStart.AutoSize = True
         Me.BtnStart.BackColor = System.Drawing.SystemColors.MenuHighlight
         Me.BtnStart.ForeColor = System.Drawing.SystemColors.Control
-        Me.BtnStart.Location = New System.Drawing.Point(142, 111)
+        Me.BtnStart.Location = New System.Drawing.Point(12, 12)
         Me.BtnStart.Name = "BtnStart"
         Me.BtnStart.Size = New System.Drawing.Size(151, 62)
         Me.BtnStart.TabIndex = 1
@@ -43,7 +51,7 @@ Partial Class Form1
         '
         Me.Btn2.BackColor = System.Drawing.Color.DarkCyan
         Me.Btn2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Btn2.Location = New System.Drawing.Point(142, 194)
+        Me.Btn2.Location = New System.Drawing.Point(12, 95)
         Me.Btn2.Name = "Btn2"
         Me.Btn2.Size = New System.Drawing.Size(151, 62)
         Me.Btn2.TabIndex = 2
@@ -54,12 +62,80 @@ Partial Class Form1
         '
         Me.Btn3.BackColor = System.Drawing.Color.DarkTurquoise
         Me.Btn3.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Btn3.Location = New System.Drawing.Point(142, 274)
+        Me.Btn3.Location = New System.Drawing.Point(12, 175)
         Me.Btn3.Name = "Btn3"
         Me.Btn3.Size = New System.Drawing.Size(151, 62)
         Me.Btn3.TabIndex = 3
         Me.Btn3.Text = "Data Types"
         Me.Btn3.UseVisualStyleBackColor = False
+        '
+        'BtnInput
+        '
+        Me.BtnInput.BackColor = System.Drawing.Color.CadetBlue
+        Me.BtnInput.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.BtnInput.Location = New System.Drawing.Point(12, 260)
+        Me.BtnInput.Name = "BtnInput"
+        Me.BtnInput.Size = New System.Drawing.Size(151, 62)
+        Me.BtnInput.TabIndex = 4
+        Me.BtnInput.Text = "Input Data"
+        Me.BtnInput.UseVisualStyleBackColor = False
+        '
+        'LabelFirstName
+        '
+        Me.LabelFirstName.AutoSize = True
+        Me.LabelFirstName.Location = New System.Drawing.Point(277, 140)
+        Me.LabelFirstName.Name = "LabelFirstName"
+        Me.LabelFirstName.Size = New System.Drawing.Size(57, 13)
+        Me.LabelFirstName.TabIndex = 5
+        Me.LabelFirstName.Text = "First Name"
+        '
+        'LabelLastName
+        '
+        Me.LabelLastName.AutoSize = True
+        Me.LabelLastName.Location = New System.Drawing.Point(277, 184)
+        Me.LabelLastName.Name = "LabelLastName"
+        Me.LabelLastName.Size = New System.Drawing.Size(58, 13)
+        Me.LabelLastName.TabIndex = 6
+        Me.LabelLastName.Text = "Last Name"
+        '
+        'LabelGender
+        '
+        Me.LabelGender.AutoSize = True
+        Me.LabelGender.Location = New System.Drawing.Point(277, 233)
+        Me.LabelGender.Name = "LabelGender"
+        Me.LabelGender.Size = New System.Drawing.Size(42, 13)
+        Me.LabelGender.TabIndex = 7
+        Me.LabelGender.Text = "Gender"
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(355, 137)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(156, 20)
+        Me.txtFirstName.TabIndex = 8
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(355, 181)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(156, 20)
+        Me.txtLastName.TabIndex = 9
+        '
+        'txtGender
+        '
+        Me.txtGender.Location = New System.Drawing.Point(355, 230)
+        Me.txtGender.Name = "txtGender"
+        Me.txtGender.Size = New System.Drawing.Size(156, 20)
+        Me.txtGender.TabIndex = 10
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(378, 282)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(111, 40)
+        Me.btnSubmit.TabIndex = 11
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
         '
         'Form1
         '
@@ -67,7 +143,15 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoSize = True
         Me.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.ClientSize = New System.Drawing.Size(435, 445)
+        Me.ClientSize = New System.Drawing.Size(861, 596)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.txtGender)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.LabelGender)
+        Me.Controls.Add(Me.LabelLastName)
+        Me.Controls.Add(Me.LabelFirstName)
+        Me.Controls.Add(Me.BtnInput)
         Me.Controls.Add(Me.Btn3)
         Me.Controls.Add(Me.Btn2)
         Me.Controls.Add(Me.BtnStart)
@@ -80,4 +164,12 @@ Partial Class Form1
     Friend WithEvents BtnStart As Button
     Public WithEvents Btn2 As Button
     Public WithEvents Btn3 As Button
+    Public WithEvents BtnInput As Button
+    Friend WithEvents LabelFirstName As Label
+    Friend WithEvents LabelLastName As Label
+    Friend WithEvents LabelGender As Label
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents txtGender As TextBox
+    Friend WithEvents btnSubmit As Button
 End Class
--- Visual Basic/Project 1/WindowsApp1/WindowsApp1/Form1.vb
+++ Visual Basic/Project 1/WindowsApp1/WindowsApp1/Form1.vb
@@ -47,4 +47,23 @@
                vbNewLine & decPrice & vbNewLine & DtDateRegistered)
 
     End Sub
+
+    Private Sub InputData_Click(sender As Object, e As EventArgs) Handles BtnInput.Click
+        Dim name As String
+        name = InputBox("Plese Enter Your Name : ")
+        MsgBox("Hello " & name)
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim stFirstName As String
+        Dim stLastName As String
+        Dim stGender As String
+
+        stFirstName = txtFirstName.Text
+        stLastName = txtLastName.Text
+        stGender = txtGender.Text
+
+        MsgBox("Hello " & stFirstName & " " & stLastName & " you are a " & stGender)
+
+    End Sub
 End Class
